c22ee67e6597fbb5934fc9271a687eccdd7b2d78
Kripto Lix <kriptolix@gmail.com>
2025-03-07 12:02:42 -0300

    Dropdowns working

--- src/gtk/widgets/weather.py
+++ src/gtk/widgets/weather.py
@@ -30,6 +30,7 @@ from ...iooperations import list_diretory_content
 class Weather(Gtk.Box):
     __gtype_name__ = 'Weather'
 
+    _scenario = Gtk.Template.Child()
     _region = Gtk.Template.Child()
     _season = Gtk.Template.Child()
     _dramatic_mode = Gtk.Template.Child()
@@ -41,68 +42,101 @@ class Weather(Gtk.Box):
 
         self._data_user_path = GLib.get_user_data_dir()
 
-        self._region_model = Gtk.StringList.new()
+        self._scenario_model = Gtk.StringList.new()
         self._season_model = Gtk.StringList.new()
+        self._region_model = Gtk.StringList.new()
 
+        self._scenario.set_model(self._scenario_model)
         self._region.set_model(self._region_model)
         self._season.set_model(self._season_model)
 
-        self._region.connect('notify::selected-item', self._item_selected)
+        self._scenario.connect('notify::selected-item', self._item_selected)
         self._season.connect('notify::selected-item', self._item_selected)
 
+        self._setup_scenario()
         self._setup_region()
-        self._setup_weather()
 
-    def _setup_region(self):
+    def _setup_scenario(self):
 
-        region_dir = Gio.File.new_for_path(
+        scenario_dir = Gio.File.new_for_path(
             self._data_user_path + "/datasets")
 
-        regions_enum = list_diretory_content(region_dir)
+        scenarios_enum = list_diretory_content(scenario_dir)
 
-        for region in regions_enum:
-            region_name = region.get_name()
+        for scenario in scenarios_enum:
+            scenario_name = scenario.get_name()
 
             language_dir = Gio.File.new_for_path(
-                self._data_user_path + "/datasets" + "/" + region_name)
+                self._data_user_path + "/datasets" + "/" + scenario_name)
 
             languages_enum = list_diretory_content(language_dir)
 
             for language in languages_enum:
                 language_name = language.get_name()
 
-                self._region_model.append(
-                    region_name.capitalize() + " - " + language_name)
+                self._scenario_model.append(
+                    scenario_name.capitalize() + " - " + language_name)
 
-    def _setup_weather(self):
+    def _setup_region(self):
 
-        items = self._season_model.get_n_items()
-        print(items)
+        items = self._region_model.get_n_items()        
 
         if items > 0:
-            
-            self._season_model.splice(0, items, None)
 
-        region_selected = self._region.get_selected_item().get_string()
+            self._region_model.splice(0, items, None)
 
-        region = region_selected.replace(" - ", "/")
-        region = region[0].lower() + region[1:]
+        scenario_selected = self._scenario.get_selected_item().get_string()
+
+        scenario = scenario_selected.replace(" - ", "/")
+        scenario = scenario[0].lower() + scenario[1:]
 
         weather_dir = Gio.File.new_for_path(
-            self._data_user_path + "/datasets/" + region + "/weather")
+            self._data_user_path + "/datasets/" + scenario + "/weather")
 
         weather_enum = list_diretory_content(weather_dir)
 
         for weather in weather_enum:
             weather_name = weather.get_name()
 
-            self._season_model.append(weather_name)
+            self._region_model.append(weather_name)
+
+    def _setup_season(self):
+
+        items = self._season_model.get_n_items()        
+
+        if items > 0:
+
+            self._season_model.splice(0, items, None)
+
+        region_selected = self._region.get_selected_item().get_string()
+        region = region_selected.replace(" - ", "/")
+        region = region[0].lower() + region[1:]
+
+        scenario_selected = self._scenario.get_selected_item().get_string()
+        scenario = scenario_selected.replace(" - ", "/")
+        scenario = scenario[0].lower() + scenario[1:]
+
+        season_dir = Gio.File.new_for_path(
+            self._data_user_path + "/datasets/" + scenario
+            + "/weather/" + region)
+
+        season_enum = list_diretory_content(season_dir)
+
+        for season in season_enum:
+            season_name = season.get_name()
+
+            self._season_model.append(season_name)
 
     def _item_selected(self, dropdown, parameter):
 
+        if dropdown == self._scenario:
+            print("scenario")
+            self._setup_region()
+            self._setup_season()
+
         if dropdown == self._region:
             print("region")
-            self._setup_weather()
+            self._setup_season()
 
         if dropdown == self._season:
             print("season")
